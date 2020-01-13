b712ee774eb30e4f372a0d8363bf0903eca79e95
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-13 13:54:45 +0100

    maj de RestCategorieController et RestFilmController

--- src/main/java/com/montparnasse/cinema/controller/RestCategorieController.java
+++ src/main/java/com/montparnasse/cinema/controller/RestCategorieController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,8 @@ import com.montparnasse.cinema.service.ICategorieService;
  * @author IN-MP-019
  *
  */
+//faire le lien avec projet Angular
+@CrossOrigin("*") 
 @RestController
 public class RestCategorieController {
 	
