a9d609ee6856fa0a3534d22ce9281474951375d7
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-13 13:46:26 +0100

    ajout de RestFilmController dans couche controller

---
+++ src/main/java/com/montparnasse/cinema/controller/RestFilmController.java
@@ -0,0 +1,75 @@
+package com.montparnasse.cinema.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.montparnasse.cinema.domaine.Film;
+import com.montparnasse.cinema.service.IFilmService;
+
+/**
+ * Classe Controller RestFilmController de la couche controller
+ * @author IN-MP-019
+ *
+ */
+@RestController
+public class RestFilmController {
+	
+	//recup du service
+	@Autowired
+	private IFilmService filmService;
+	
+	//lister ts les films
+	@RequestMapping(value = "/films", //
+					method = RequestMethod.GET, //
+					produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public List<Film> getFilms() {
+		List<Film> list = filmService.recupererTout();
+		return list;
+	}//end getFilms
+	
+	//lister un film
+	@RequestMapping(value = "/films/{idFilmA}", //
+					method = RequestMethod.GET, //
+					produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public Film getFilm(@PathVariable("idFilmA") Long idFilm) {
+		return filmService.recuperer(idFilm);
+	}//end getFilm
+	
+	//ajouter un film
+	@RequestMapping(value = "/films", //
+					method = RequestMethod.POST, //
+					produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public Film addFilm(@RequestBody Film film) {
+		return filmService.ajouter(film);
+	}//end addFilm
+	
+	//modifier un film
+	@RequestMapping(value = "/films", //
+					method = RequestMethod.PUT, //
+					produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public Film updateFilm(@RequestBody Film film) {
+		return filmService.modifier(film);
+	}//end updateFilm
+	
+	//supprimer un film
+	@RequestMapping(value = "/films/{idFilmA}", //
+					method = RequestMethod.DELETE, //
+					produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public boolean deleteFilm(@PathVariable("idFilmA") Long idFilm) {
+		return filmService.supprimer(idFilm);
+	}//end deleteFilm
+
+}//end class
