054bcdebcdea57a0d9576d79ff97a8ab9ae418f0
Wendy TOKO <wendytoko@outlook.fr>
2020-01-13 12:12:37 +0100

    création de CinemaServiceImpl

---
+++ src/main/java/com/montparnasse/cinema/service/CinemaServiceImpl.java
@@ -0,0 +1,47 @@
+package com.montparnasse.cinema.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.montparnasse.cinema.dao.ICinemaDao;
+import com.montparnasse.cinema.domaine.Cinema;
+
+@Service
+public class CinemaServiceImpl implements ICinemaService{
+
+	@Autowired
+	private ICinemaDao idao;
+	
+	@Override
+	public Cinema ajouter(Cinema c) {
+		// TODO Auto-generated method stub
+		return idao.save(c);
+	}
+
+	@Override
+	public Cinema recuperer(Long id) {
+		// TODO Auto-generated method stub
+		return idao.findById(id).get();
+	}
+
+	@Override
+	public Cinema modifier(Cinema c) {
+		// TODO Auto-generated method stub
+		return idao.save(c);
+	}
+
+	@Override
+	public boolean supprimer(Cinema c) {
+		idao.delete(c);
+		return true;
+	}
+
+	@Override
+	public List<Cinema> recupererTout() {
+		// TODO Auto-generated method stub
+		return idao.findAll();
+	}
+
+}
