5f6ca0a94b3d5ab77cb4e5c8c3d1b06780de40df
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-13 12:40:06 +0100

    ajout IFilmService et FilmServiceImpl dans service

---
+++ src/main/java/com/montparnasse/cinema/service/IFilmService.java
@@ -0,0 +1,13 @@
+package com.montparnasse.cinema.service;
+
+import com.montparnasse.cinema.domaine.Film;
+
+/**
+ * interface IFilmService de la couche service
+ * hérite de l'interface générique IGeneriqueService
+ * @author IN-MP-019
+ *
+ */
+public interface IFilmService extends IGeneriqueService<Film>{
+
+}//end interface
