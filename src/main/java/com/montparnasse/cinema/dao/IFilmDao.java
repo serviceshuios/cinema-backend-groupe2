db93eaeb8c492d6d2eb01db61666e157671395f5
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-13 11:53:08 +0100

    ajout de ICategorieDao, IFilmDao et IProjectionDao de couche dao

---
+++ src/main/java/com/montparnasse/cinema/dao/IFilmDao.java
@@ -0,0 +1,19 @@
+package com.montparnasse.cinema.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.montparnasse.cinema.domaine.Film;
+
+/**
+ * interface IFilmDao de la couche dao
+ * @author IN-MP-019
+ *
+ */
+@Transactional
+@Repository
+public interface IFilmDao extends JpaRepository<Film, Long> {
+
+}//end interface
