5f476e1698e33e31b290a1ad3afa0015d1c6dc22
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-13 11:13:08 +0100

    ajout de la classe Categorie dans la couche domaine

--- src/main/java/com/montparnasse/cinema/domaine/Categorie.java
+++ src/main/java/com/montparnasse/cinema/domaine/Categorie.java
@@ -2,14 +2,22 @@ package com.montparnasse.cinema.domaine;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Classe Categorie de la couche domaine
  * @author IN-MP-019
  *
  */
+@Entity
 public class Categorie implements Serializable{
 	
 	/*__________________________________ Props ____________________________________*/
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCategorie;
 	private String name;
 	
