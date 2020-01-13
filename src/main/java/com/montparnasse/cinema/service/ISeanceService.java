086fce748bb763fb0cef4469b2b71ab0da7455e5
Adrien RIVIERE <ad.riv@hotmail.fr>
2020-01-13 12:39:31 +0100

    Creation de IPlaceService, ITicketService, ISeanceService de service

---
+++ src/main/java/com/montparnasse/cinema/service/ISeanceService.java
@@ -0,0 +1,13 @@
+package com.montparnasse.cinema.service;
+
+import com.montparnasse.cinema.domaine.Seance;
+
+/**
+ * interface ISeanceService de la couche service
+ * hérite de l'interface générique IGeneriqueService
+ * @author IN-MP-019
+ *
+ */
+public interface ISeanceService extends IGeneriqueService<Seance>{
+
+}//end interface
---
+++ src/main/java/com/montparnasse/cinema/service/ITicketService.java
@@ -0,0 +1,13 @@
+package com.montparnasse.cinema.service;
+
+import com.montparnasse.cinema.domaine.Ticket;
+
+/**
+ * interface ITicketService de la couche service
+ * hérite de l'interface générique IGeneriqueService
+ * @author IN-MP-019
+ *
+ */
+public interface ITicketService extends IGeneriqueService<Ticket> {
+
+}//end interface
