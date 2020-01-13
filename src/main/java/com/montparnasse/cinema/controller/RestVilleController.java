03f2730b732833009759d48a84552d0c256d88f1
Wendy TOKO <wendytoko@outlook.fr>
2020-01-13 12:43:37 +0100

    création de RestVilleController dans controller

---
+++ src/main/java/com/montparnasse/cinema/controller/RestVilleController.java
@@ -0,0 +1,73 @@
+package com.montparnasse.cinema.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.montparnasse.cinema.domaine.Ville;
+import com.montparnasse.cinema.service.IVilleService;
+
+@CrossOrigin("*")
+@RestController
+public class VilleController {
+
+	@Autowired
+	private IVilleService service;
+
+	// ======= Liste des villes ========= //
+
+	@RequestMapping(value = "/villes", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public List<Ville> recupererTout() {
+		List<Ville> listeVilles = service.recupererTout();
+		return listeVilles;
+	}
+
+	// ====== Recupérer une ville ========== //
+
+	@RequestMapping(value = "/villes/{idVille}", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public Ville recuperer(@PathVariable("idVille") Long id) {
+		Ville ville = service.recuperer(id);
+		return ville;
+	}
+
+	// ====== Recupérer une ville ========== //
+
+	@RequestMapping(value = "/villes", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public void ajouter(@RequestBody Ville v) {
+		service.ajouter(v);
+	}
+
+	// ====== Modifier une ville ========== //
+
+	@RequestMapping(value = "/villes", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public Ville modifier(@RequestBody Ville v) {
+
+		return service.modifier(v);
+
+	}
+
+	// ====== Supprimer une ville ========== //
+
+	@RequestMapping(value = "/villes/{idVille}", method = RequestMethod.DELETE, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public boolean supprimer(@PathVariable("idVille") Long id) {
+
+		return service.supprimer(id);
+
+	}
+
+}
