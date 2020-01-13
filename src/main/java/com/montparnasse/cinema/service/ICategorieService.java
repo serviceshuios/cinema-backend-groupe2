c7a86a41e7d0088c83e57690e88ebaa334f157fa
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-13 12:29:23 +0100

    ajout de ICategorieService et CategorieService dans service

---
+++ src/main/java/com/montparnasse/cinema/service/ICategorieService.java
@@ -0,0 +1,13 @@
+package com.montparnasse.cinema.service;
+
+import com.montparnasse.cinema.domaine.Categorie;
+
+/**
+ * interface ICategorieService de la couche service
+ * hérite de l'interface générique IGeneriqueService
+ * @author IN-MP-019
+ *
+ */
+public interface ICategorieService extends IGeneriqueService<Categorie>{
+
+}//end interface
