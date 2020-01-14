f9546f90d95a236812d7c6603481e15adb5eace5
SMORTAZA <shogofa.mortaza@hotmail.fr>
2020-01-14 12:31:41 +0100

    ajout de @CrossOrigin("*") dans 3 Rest*Controller

--- src/main/java/com/montparnasse/cinema/controller/RestTicketController.java
+++ src/main/java/com/montparnasse/cinema/controller/RestTicketController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.montparnasse.cinema.service.ITicketService;
 * @author IN-MP-019
 *
 */
+@CrossOrigin("*")
 @RestController
 public class RestTicketController {
 	
